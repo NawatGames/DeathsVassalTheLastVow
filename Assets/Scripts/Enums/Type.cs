409f2466eea1a0f14bf11d3169bf19380a07e1e5
Ilan Hameiry <Ilan.hame@gmail.com>
2026-03-22 21:39:48 -0300

    feat: add Type enum with flags

---
+++ Assets/Scripts/Enums/Type.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[System.Flags]
+public enum Type
+{
+    Null = 0,
+    Draconideo = 1 << 0,
+    Humanoide = 1 << 1,
+    Fera = 1 << 2,
+    MortoVivo = 1 << 3,
+    Monstro = 1 << 4,
+    Espirito = 1 << 5
+}
