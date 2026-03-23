137ce557a677baaedf8ff0a33908163e0b0e693b
Ilan Hameiry <Ilan.hame@gmail.com>
2026-03-22 22:15:37 -0300

    refactor: set the value of each enum constant in Mythology

--- Assets/Scripts/Enums/Mythology.cs
+++ Assets/Scripts/Enums/Mythology.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 [Serializable]
 public enum Mythology
 {
-    Inicial,
-    Brasil,
-    Chinesa,
-    Egipcia,
-    Grega,
-    Japonesa,
-    Nordica,
-    Null
+    Inicial = 0,
+    Brasil = 1,
+    Chinesa = 2,
+    Egipcia = 3,
+    Grega = 4,
+    Japonesa = 5,
+    Nordica = 6,
+    Null = 7
 
 }
